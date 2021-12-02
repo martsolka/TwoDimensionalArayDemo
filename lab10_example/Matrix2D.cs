864d8f1cb1c1e5b85202a0e479d352326bde7ca1
Solomia <solka.marsolka@gmail.com>
2021-12-02 19:12:03 +0200

    Додала 4 методи для роботи з масивом

--- lab10_example/Matrix2D.cs
+++ lab10_example/Matrix2D.cs
@@ -9,6 +9,7 @@ namespace lab10_example
     class Matrix2D
     {
         int[,] matrix;
+        int[] absolutRowsSum;
 
         public Matrix2D(int rows, int cols)
         {
@@ -33,6 +34,58 @@ namespace lab10_example
             for (int i = 0; i < RowCount; i++)
                 for (int j = 0; j < ColCount; j++)
                     matrix[i, j] = rand.Next(min, max + 1);
+        }
+
+        //Шукає перший стовпець, що не містять відємних елементів
+        //повертає індекс стовпця або -1, якщо такого стовпця не знайдено
+        public int IndexOfFirstPositiveColumn()
+        {
+            for (int i = 0; i < ColCount; i++)
+            {
+                int positiveElementsCount = 0;
+                for (int j = 0; j < RowCount; j++)
+                {
+                    if (matrix[j, i] >= 0)
+                        positiveElementsCount++;
+                    else
+                        break;
+                }
+                if (positiveElementsCount == RowCount)
+                    return i;
+            }
+            return -1;
+        }
+        // Сортування рядків матриці за зростанням
+        // суми абсолютних значень елементів рядків
+        public void SortRowsByCharacteristic()
+        {
+            if (absolutRowsSum == null)
+                СomputeAbsolutRowsSum();
+            for (int i = 0; i < RowCount; i++)
+                for (int j = i + 1; j < RowCount; j++)
+                    if (absolutRowsSum[i] > absolutRowsSum[j])
+                    {
+
+                        for (int k = 0; k < ColCount; k++)
+                            Swap(ref matrix[i, k], ref matrix[j, k]);
+                        Swap(ref absolutRowsSum[i], ref absolutRowsSum[j]);
+                    }
+        }
+        // Обчислення абсолютної суми елементів кожного рядка
+        // повертає одновимірний масив зі значенням суми у кожному рядку
+        public void СomputeAbsolutRowsSum()
+        {
+            absolutRowsSum = new int[RowCount];
+            for (int i = 0; i < RowCount; i++)
+                for (int j = 0; j < ColCount; j++)
+                    absolutRowsSum[i] += Math.Abs(matrix[i, j]);
+        }
+        // Міняє місцями значення двох змінних
+        private void Swap(ref int firstElement, ref int secondElement)
+        {
+            int tempElement = firstElement;
+            firstElement = secondElement;
+            secondElement = tempElement;
         }
     }
 }
